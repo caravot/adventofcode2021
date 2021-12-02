9de4cfbfcdb3538d20dd6d17de5cebf94f5b13e3
Carrie Ravotta <carrie.peary@gmail.com>
2021-12-02 14:20:28 -0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,6 +1,7 @@
 # part 1
 horizontal_position = 0
 depth = 0
+aim = 0
 
 with open('input.txt') as f:
     for line in f.readlines():
@@ -15,3 +16,17 @@ with open('input.txt') as f:
             depth -= num
 
 print(horizontal_position * depth)
+
+# part 2
+with open('input.txt') as f:
+    for line in f.readlines():
+        direction, num = line.split(' ')
+        num = int(num)
+        
+        if direction == 'forward':
+            horizontal_position += num
+            depth += aim * num
+        elif direction == 'down':
+            aim += num
+        elif direction == 'up':
+            aim -= num
