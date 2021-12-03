1b03963bf2a08a2c122566ceb01ac7e0e4f705cd
Carrie Kumar <carrie.kumar@inovexcorp.com>
2021-12-02 20:09:24 -0500

    Finished Day 1 part 2

--- 1.py
+++ 1.py
@@ -1,11 +1,27 @@
+#part 1
+
 last_depth = 0
 depth_increased = 0
 
-with open('1.txt') as f:
+with open('1_example.txt') as f:
     for line in f:
         current_depth = int(line)
         if last_depth <= current_depth and last_depth > 0:
             depth_increased += 1
         last_depth = current_depth
+    f.close()
 
-print(depth_increased)
+print("part1: ", depth_increased)
+print("------------------")
+
+# part 2
+# I cheated :(
+last_measure_count = 0
+depth_increased = 0
+
+with open('1.txt') as f:
+    nums = f.readlines()
+    n = list(map(int, nums))
+    print(sum(a < b for a, b in zip(n, n[3:])))
+
+    f.close()
--- 2.py
+++ 2.py
@@ -1,13 +1,12 @@
 # part 1
 horizontal_position = 0
 depth = 0
-aim = 0
 
-with open('input.txt') as f:
+with open('2.txt') as f:
     for line in f.readlines():
         direction, num = line.split(' ')
         num = int(num)
-        
+
         if direction == 'forward':
             horizontal_position += num
         elif direction == 'down':
@@ -17,12 +16,16 @@ with open('input.txt') as f:
 
 print(horizontal_position * depth)
 
+horizontal_position = 0
+depth = 0
+aim = 0
+
 # part 2
-with open('input.txt') as f:
+with open('2.txt') as f:
     for line in f.readlines():
         direction, num = line.split(' ')
         num = int(num)
-        
+
         if direction == 'forward':
             horizontal_position += num
             depth += aim * num
@@ -30,3 +33,6 @@ with open('input.txt') as f:
             aim += num
         elif direction == 'up':
             aim -= num
+
+print(horizontal_position, depth)
+print(horizontal_position * depth)
